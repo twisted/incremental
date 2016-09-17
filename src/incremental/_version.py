8e5cbbfbab70bad1ff04a737abdab71082a3724c
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-09-18 01:15:43 +0800

    bump to rc

--- src/incremental/_version.py
+++ src/incremental/_version.py
@@ -1,2 +1,7 @@
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update Incremental` to change this file.
+
 from incremental import Version
-__version__ = Version("Incremental", 15, 3, 0)
+
+__version__ = Version('Incremental', 16, 9, 0, release_candidate=1)
+__all__ = ["__version__"]
