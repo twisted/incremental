986f7830c5d820d3960e6eebf7ecee55c4f67dca
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2017-03-12 14:08:19 +1100

    structure

---
+++ src/eventually/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides Eventually version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update Eventually` to change this file.
+
+from incremental import Version
+
+__version__ = Version('Eventually', 17, 3, 0)
+__all__ = ["__version__"]
