c22c9f96c66ad6c379124603cc5072131c6d1d6c
Amber Brown <amber@atleastfornow.net>
2017-05-20 10:57:25 -0700

    change url

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     version=incremental.__version__.base(),
     maintainer='Amber Brown',
     maintainer_email='hawkowl@twistedmatrix.com',
-    url="https://github.com/hawkowl/incremental",
+    url="https://github.com/twisted/incremental",
     classifiers = [
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
